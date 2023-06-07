51e955119a30d834ed2bdd6d8cdeb8b90adf6539
Wolfframio <116112114+Wolfframio@users.noreply.github.com>
2023-06-07 23:57:27 +0200

    Add files via upload

---
+++ iteraciones.py
@@ -0,0 +1,117 @@
+"""
+1. Crear un programa que imprima una secuencia de números pares, que
+empezando por el 0, su número más alto sea menor que 30
+"""
+
+
+
+"""
+2. Supongamos el siguiente programa:
+iteracion = 0
+contador = 0
+while iteracion < 5:
+ # la variable letra en el loop, itera sobre todos los caracteres
+ # de la cadena, incluidas comas, espacios y símbolos
+
+ for letra in "¡Hola mundo!":
+ contador += 1
+ iteracion += 1
+Sin ejecutar el código en Python, analiza el código y responde a las cuestiones.
+1) ¿Cuál es el valor de la variable contador durante la iteración 0?
+2) ¿Cuál es el valor de la variable contador durante la iteración 1?
+3) ¿Cuál es el valor de la variable contador durante la iteración 2?
+4) ¿Cuál es el valor de la variable contador durante la iteración 3?
+5) ¿Cuál es el valor de la variable contador durante la iteración 4?
+"""
+
+
+
+"""
+3. Supongamos el siguiente programa:
+iteracion = 0
+while iteracion < 5:
+ contador = 0
+ for letra in "¡Hola mundo!":
+ contador += 1
+ iteracion += 1
+1) ¿Cuál es el valor de la variable contador durante la iteración 0?
+2) ¿Cuál es el valor de la variable contador durante la iteración 1?
+3) ¿Cuál es el valor de la variable contador durante la iteración 2?
+4) ¿Cuál es el valor de la variable contador durante la iteración 3?
+5) ¿Cuál es el valor de la variable contador durante la iteración 4?
+"""
+
+
+
+"""
+4. Supongamos el siguiente programa:
+iteracion = 0
+while iteracion < 5:
+ contador = 0
+ for letra in "¡Hola mundo!":
+ contador += 1
+ if iteracion % 2 == 0:
+ break
+ iteracion += 1
+1) ¿Cuántas veces se ejecuta el comando print?
+2) ¿Cuál es el máximo valor que toma la variable iteracion?
+3) ¿Cuál es el máximo valor de la variable contador?
+4) ¿Cuál es el menor valor de la variable contador?
+"""
+
+
+
+"""
+5. Crear un programa que tras introducir un número, nos diga si dicho número tiene
+cubo perfecto o no.
+Nota: 8 cubo perfecto de 2, 27 cubo perfecto de 3, 64 cubo perfecto de 4, …
+"""
+
+
+
+"""
+6. Crea un programa que añada elementos a una lista de manera indefinida,
+mientras pulsemos una tecla.
+NOTA: Cada vez que introduzcamos un elemento en la lista, nos deberá preguntar
+si queremos seguir añadiendo elementos, si no pulsemos una tecla en concreto,
+el proceso terminará.
+"""
+
+
+"""
+7. Crea un programa que calcule el factorial de un número.
+Nota: Factorial de 4 = 4! = 4 * 3 * 2 * 1
+Factorial de 5 = 5! = 5 * 4 * 3 * 2 * 1
+"""
+
+
+
+"""
+8. Crear un programa que extráiga el total de vocales existentes en una cadena de
+texto.
+NOTA: El programa nos pedirá introducir una cadena de texto y nos devolverá el
+total de vocales. Para simplificar la resolución del ejercicio, asegúrate de
+convertir la cadena de texto a analizar a minúsculas.
+"""
+
+
+
+
+"""
+9. Crear un programa que nos diga si una cadena de texto, contiene las vocales “i”
+y “u”.
+"""
+
+
+"""
+10.Desarrollar un programa que dado un número de mes (1 al 12), nos diga el
+nombre del mes y liste los 11 siguientes meses.
+Pej, si introducimos un 8, el programa deberá decir Agosto, e imprimir en pantalla
+Agosto, Septiembre, Octubre, Noviembre, Diciembre, Enero, ... , Julio
+"""
+
+
+"""
+11.Desarrollar un programa que dada una cadena de texto, la invierta. Intenta
+plantear el ejercicio con WHILE y FOR
+"""
